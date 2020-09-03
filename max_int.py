57f130a90ddb2f8887143234414cca4f164cdef2
ThorRafnar <62342382+ThorRafnar@users.noreply.github.com>
2020-09-03 13:41:37 +0000

    Add files via upload

---
+++ max_int.py
@@ -0,0 +1,6 @@
+#Ask user for a number, repeat as long as number is positive
+#Store number as max_int 
+#check if new input is larger than max_int
+#if yes, overwrite max_int
+#else, forget it
+#when a negative value is added, print max_int and exit
